497b51aff1129c993c41e690b3e3cf5b68278829
Tzuan1020 <99935739+Tzuan1020@users.noreply.github.com>
2024-06-11 16:41:14 +0800

    Create hw8.py

---
+++ hw8.py
@@ -0,0 +1,23 @@
+#參考111010529顏瑋成同學所寫出
+import gymnasium as gym
+env = gym.make("CartPole-v1", render_mode="human") # 若改用這個，會畫圖
+# env = gym.make("CartPole-v1", render_mode="rgb_array")
+observation, info = env.reset(seed=42)
+score = 0
+#當竿子的角速度大於0的時候向右，反之向左
+def action(observation):
+    if observation[3]>0:
+        action = 1
+    else:
+        action = 0
+    return action
+for _ in range(1000):
+   env.render()
+   observation, reward, terminated, truncated, info = env.step(action(observation))
+   #print('observation=', observation)
+   score += reward
+   if terminated or truncated:
+      observation, info = env.reset()
+      print('done, score=', score)
+      score = 0
+env.close()
